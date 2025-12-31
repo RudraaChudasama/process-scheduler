b3534c2a80a0f0ce69098f11252cb0e8e592f09c
Rudra chudasama <rudra.cict22@gmail.com>
2025-12-31 11:34:36 +0530

    sjf.cpp

---
+++ sjf.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    int burst[n], waiting[n], turnaround[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter burst time for P" << i + 1 << ": ";
+        cin >> burst[i];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (burst[i] > burst[j]) {
+                swap(burst[i], burst[j]);
+            }
+        }
+    }
+
+    waiting[0] = 0;
+    for (int i = 1; i < n; i++) {
+        waiting[i] = waiting[i - 1] + burst[i - 1];
+    }
+
+    cout << "\nProcess\tBurst\tWaiting\tTurnaround\n";
+    for (int i = 0; i < n; i++) {
+        turnaround[i] = waiting[i] + burst[i];
+        cout << "P" << i + 1 << "\t" << burst[i]
+             << "\t" << waiting[i]
+             << "\t" << turnaround[i] << endl;
+    }
+
+    return 0;
+}
